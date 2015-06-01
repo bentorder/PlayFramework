d8dbcb2d012e064a6067e35e8b935d9051e2a76f
masta05 <nayaneko@softbank.ne.jp>
2015-06-01 22:02:45 +0900

    firstProject

---
+++ firstProject/app/controllers/Application.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import java.util.List;
+
+import models.Parent;
+import play.db.ebean.Model.Finder;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
+public class Application extends Controller {
+
+    public static Result index() {
+
+        // 現在のParentを全検索して表示
+        Finder<Long, Parent> finder = new Finder<Long, Parent>(Long.class,
+                Parent.class);
+        List<Parent> parents = finder.all();
+
+        return ok(index.render(parents));
+    }
+}
